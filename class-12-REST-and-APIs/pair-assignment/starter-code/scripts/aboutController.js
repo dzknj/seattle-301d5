43059b5105fb718402497df43bd62ba8b4932997
David Michael Zalk <dzknj@outlook.com>
2016-02-16 13:06:29 -0800

    driver switch

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/aboutController.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/aboutController.js
@@ -6,7 +6,7 @@
 
     // TODO: Call a function to load all the data.
     // Pass a view function as a callback, so the view will render after the data is loaded.
-
+    repos.somerepomethod(call view method to render repos)
   };
 
   module.aboutController = aboutController;
