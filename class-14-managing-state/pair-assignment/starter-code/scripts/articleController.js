c778cf5f8ee7c3993334fd7a14250bd3f15a4bcd
David Michael Zalk <dzknj@outlook.com>
2016-02-18 13:55:13 -0800

    commented on the articleController page

--- class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
+++ class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
@@ -6,23 +6,35 @@
   articlesController.index = function(ctx, next) {
     articleView.index(ctx.articles);
   };
-
-  // COMMENT:This method is first ran by routes when the endpoint /article/:id is hit.
-  // It takes context (ctx) and next as parameters. Ctx is the object that is assigned
-  // the properties we are working with and stored for later use. And Next is the callback
-  // function representing the next funtion. We set ctx.articles
+/*
+  COMMENT:This method is first ran by routes when the endpoint /article/:id is hit.
+  It takes context (ctx) and next as parameters. Ctx is the object that is assigned
+  the properties we are working with and stored for later use. And Next is the callback
+  function representing the next funtion. Article.findWhere takes the ID
+  which is after /article/ and inputs it into the findWhere function.
+  It than searches through the context object for where ctx.params.id matches the ID.
+  It than runs articlData as a callback. articleData sets ctx.articles equal to the
+  article returned by the fidWhere search, and than the next() function is called.
+*/
   articlesController.loadById = function(ctx, next) {
     var articleData = function(article) {
       ctx.articles = article;
-      console.log(article);
-      console.log(ctx);
       next();
     };
 
     Article.findWhere('id', ctx.params.id, articleData);
   };
-
-  // COMMENT: What does this method do?  What is it's execution path?
+/*
+  COMMENT:This method is first ran by routes when the endpoint /author/:authorName is hit.
+  It takes context (ctx) and next as parameters. Ctx is the object that is assigned
+  the properties we are working with and stored for later use. And Next is the callback
+  function representing the next funtion. Article.findWhere takes the author
+  which is after /author/ and inputs it into the findWhere function.
+  It than searches through the context object for where ctx.params.authorName matches the author.
+  The .replace changes the + in the data to a space for searching purposes.
+  It than runs authorData as a callback. authorData sets ctx.articles equal to the
+  articles returned by the findWhere search, and than the next() function is called.
+*/
   articlesController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
@@ -32,7 +44,16 @@
     Article.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData);
   };
 
-  // COMMENT: What does this method do?  What is it's execution path?
+/*
+  COMMENT:This method is first ran by routes when the endpoint /category/:categoryName is hit.
+  It takes context (ctx) and next as parameters. Ctx is the object that is assigned
+  the properties we are working with and stored for later use. And Next is the callback
+  function representing the next funtion. Article.findWhere takes the category
+  which is after /category/ and inputs it into the findWhere function.
+  It than searches through the context object for where ctx.params.categoryName matches the category.
+  It than runs categoryData as a callback. categoryData sets ctx.articles equal to the
+  articles returned by the findWhere search, and than the next() function is called.
+*/
   articlesController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
       ctx.articles = articlesInCategory;
@@ -42,8 +63,15 @@
     Article.findWhere('category', ctx.params.categoryName, categoryData);
   };
 
-  // COMMENT: What does this method do?  What is it's execution path?
-  articlesController.loadAll = function(ctx, next) {
+/*
+  COMMENT:This method is first ran by routes when the endpoint '/' is hit.
+  It takes context (ctx) and next as parameters. Ctx is the object that is assigned
+  the properties we are working with and stored for later use. And Next is the callback
+  function representing the next funtion. If Articles.all has a length of greater than zero,
+  ctx.articles is set to the Article.all array values and the next() callback is run.
+  If the above is not true, then the Article.fetchAll function is run with articleData as the
+  next callback. Article.fetchAll grabs all the articles and puts them on the page. 
+*/  articlesController.loadAll = function(ctx, next) {
     var articleData = function(allArticles) {
       ctx.articles = Article.all;
       next();
